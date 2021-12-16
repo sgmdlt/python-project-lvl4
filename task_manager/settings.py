3ee3f076e13c36a391490f00e4c880d15f25113f
Ivan Mamtsev <justgodshot@gmail.com>
2021-12-16 15:48:37 +0300

    fix linter error

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = os.getenv('DEBUG', False) == 'True'
+DEBUG = os.getenv('DEBUG', False) == 'True'  # noqa: WPS425
 
 ALLOWED_HOSTS = [
     'localhost',
