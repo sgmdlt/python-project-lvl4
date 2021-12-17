99c0c452cb0a569a8589475819cc6f98c2a90c4d
Ivan Mamtsev <justgodshot@gmail.com>
2021-12-17 16:32:27 +0300

    turn off admin view

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,8 +1,6 @@
-from django.contrib import admin
 from django.urls import path
 from task_manager import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', views.index, name='index'),
 ]
