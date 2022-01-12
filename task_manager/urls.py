19399b5caf315aed0a4c6f395855e756964ec6b5
Ivan Mamtsev <justgodshot@gmail.com>
2022-01-12 23:04:48 +0300

    add home page view

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from task_manager import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.HomeView.as_view(), name='index'),
 ]
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import HttpResponse
+from django.views.generic.base import TemplateView
 
 
-def index(request):
-    return HttpResponse('Hello from Task Manager!')
+class HomeView(TemplateView):
+
+    template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        return context
