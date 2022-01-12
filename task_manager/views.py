2e8195efeacf9fe3620031be56b22d0abecb26c3
Ivan Mamtsev <justgodshot@gmail.com>
2022-01-12 23:11:44 +0300

    linter fix

--- task_manager/views.py
+++ task_manager/views.py
@@ -7,4 +7,4 @@ class HomeView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        return context
+        return context  # noqa: WPS331
