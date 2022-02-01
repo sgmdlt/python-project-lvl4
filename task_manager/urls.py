2439a1d6c870c78eeca346cfc14c998453d7d209
Ivan Mamtsev <justgodshot@gmail.com>
2022-02-01 18:51:52 +0300

    connect users app

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import include, path
 from task_manager import views
 
 urlpatterns = [
+    path('admin/', admin.site.urls),
     path('', views.HomeView.as_view(), name='home'),
+    path('users/', include('users.urls')),
 ]
