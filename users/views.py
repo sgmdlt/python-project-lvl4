3b67f43d0015e73f20e6b32cf654858c34dadcd2
Ivan Mamtsev <justgodshot@gmail.com>
2022-02-22 19:30:32 +0300

    fix users view

--- users/views.py
+++ users/views.py
@@ -9,5 +9,4 @@ class UsersView(ListView):
     model = User
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
+        return super().get_context_data(**kwargs)
