74577a518f693d52240d4a7434709ed6450dcf4b
Ivan Mamtsev <justgodshot@gmail.com>
2022-02-26 23:03:09 +0300

    add user registration form

---
+++ users/forms.py
@@ -0,0 +1,9 @@
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+class CustomUserCreationForm(UserCreationForm):
+    """Site user creation form."""
+
+    class Meta(UserCreationForm.Meta):
+        model = CustomUser
+        fields = UserCreationForm.Meta.fields + ('first_name', 'last_name',)
