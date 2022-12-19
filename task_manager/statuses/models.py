82541685e049fd383c7ecb60de8c546846219583
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2022-12-19 19:07:13 +0400

    fix localisation

--- task_manager/statuses/models.py
+++ task_manager/statuses/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 MAX_LENGTH = 50
 
 
 class Status(models.Model):
-    name = models.CharField(max_length=MAX_LENGTH)
-    created_at = models.DateTimeField(auto_now=True)
+    name = models.CharField(_("name"), max_length=MAX_LENGTH)
+    created_at = models.DateTimeField(_("created at"), auto_now=True)
 
     def __str__(self):
         return self.name
--- task_manager/tasks/models.py
+++ task_manager/tasks/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 from task_manager.labels.models import Label
 from task_manager.statuses.models import Status
 
@@ -7,22 +8,30 @@ MAX_LENGTH = 50
 
 
 class Task(models.Model):
-    name = models.CharField(max_length=MAX_LENGTH)
-    description = models.TextField()
-    status = models.ForeignKey(Status, on_delete=models.PROTECT, null=True)
+    name = models.CharField(_("name"), max_length=MAX_LENGTH)
+    description = models.TextField(_("description"))
+    status = models.ForeignKey(
+        Status,
+        verbose_name=_("status"),
+        on_delete=models.PROTECT,
+        null=True,
+    )
     creator = models.ForeignKey(
         get_user_model(),
+        verbose_name=_("creator"),
         on_delete=models.PROTECT,
         related_name="creator",
     )
     executor = models.ForeignKey(
         get_user_model(),
+        verbose_name=_("executor"),
         on_delete=models.PROTECT,
         related_name="executor",
     )
-    created_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(_("created at"), auto_now=True)
     labels = models.ManyToManyField(
         Label,
+        verbose_name=_("labels"),
         blank=True,
         related_name="labels",
         through="TaskLabel",
