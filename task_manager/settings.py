36c0acf78ab4ada787d03b5b41a0516308e10551
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2022-12-19 17:02:28 +0400

    fix localisation

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -115,7 +115,7 @@ AUTH_USER_MODEL = "users.User"
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
-LANGUAGE_CODE = "ru-RU"
+LANGUAGE_CODE = "ru-ru"
 
 TIME_ZONE = "UTC"
 
