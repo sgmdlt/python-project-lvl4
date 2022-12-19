b2b86854acd34699e749633f005e6b4007e76dd8
Ivan Mamtsev <ivan.mamtsev@hexlet.io>
2022-12-19 17:47:32 +0400

    remove whitenoise

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -23,7 +23,6 @@ ALLOWED_HOSTS = [
 
 INSTALLED_APPS = [
     "bootstrap4",
-    "whitenoise.runserver_nostatic",
     "django_extensions",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -41,7 +40,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -128,8 +126,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- tests/conftest.py
+++ tests/conftest.py
@@ -4,19 +4,6 @@ from pathlib import Path
 import pytest
 
 
-@pytest.fixture(autouse=True)
-def whitenoise_autorefresh(settings):
-    """
-    Get rid of whitenoise "No directory at" warning, as it's not helpful when running tests.
-
-    Related:
-        - https://github.com/evansd/whitenoise/issues/215
-        - https://github.com/evansd/whitenoise/issues/191
-        - https://github.com/evansd/whitenoise/commit/4204494d44213f7a51229de8bc224cf6d84c01eb
-    """
-    settings.WHITENOISE_AUTOREFRESH = True
-
-
 def get_fixture(filename):
     current_dir = Path(__file__).absolute().parent
     return current_dir / "fixtures" / filename
